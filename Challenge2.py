946187a0277b77b68452a8c1699588b30402acee
spark2982 <spark2982@gmail.com>
2023-03-25 21:32:15 +1100

    Challenge 1

--- Challenge2.py
+++ Challenge2.py
@@ -1,27 +1,44 @@
-# input coefficient of restitution and initial height in m
-# report how many times ball bounces when dropped from height
-# before it rises to a height of less than 10 cm
-# also report total distance travelled by the ball
-
 # coRe = Coefficient of Restitution
-# tennis ball, basketball, super ball and softball respectively
+# dictionary for tennis ball, basketball, super ball and softball respectively
 tBall = 0.7
 bBall = 0.75
 supBall = 0.9
 sofBall = 0.3
 
-coRe = {"Tennis ball" : tBall,
-        "Basketball"  : bBall,
-        "Super ball"  : supBall,
-        "Softball"   : sofBall}
+coRe = {
+        "Tennis ball": tBall,
+        "Basketball": bBall,
+        "Super ball": supBall,
+        "Softball": sofBall
+        }
 
-ball = input("Which ball would you like to bounce? ")
+# Resetting variables
+bounce = 0
+height = 0
+distance = 0
+
+# Choosing a ball
+ball = input("Which ball would you like to bounce?" 
+             "\nOptions - Tennis ball, Basketball, Super ball, Softball"
+             "\n")
 if ball in coRe:
-    coReAns = coRe[ball]
-print("You have chosen", [ball],".")
+    coReChoice = coRe[ball]
+    print("You have chosen", ball, ".")
+else:
+    print("Sorry, that isn't an option or you have spelt it wrong.")
+    exit()
 
+# Choosing height
+initialHeight = int(input("What is the initial height in meters? "))
+height = initialHeight
 
+# bouncing loop
+while initialHeight >= .1:
+    bounce += 1
+    afterBounce = coRe[ball] * initialHeight
+    distance += afterBounce * 2
+    initialHeight = afterBounce
 
-height = int(input("What is the height of the initial height in meters? "))
-
-numOfBounce = height *
+# Rounding the answer instead of every bounce
+finalDistance = round(distance + height, 2)
+print("The", ball, "bounced", bounce, "times, and travelled", finalDistance, "meters.")

bb8f772dce662a9075e60ebc708be2c96e57d24f
spark2982 <spark2982@gmail.com>
2023-03-27 05:29:47 +1100

    Challenge 1

--- Challenge2.py
+++ Challenge2.py
@@ -41,4 +41,7 @@ while initialHeight >= .1:
 
 # Rounding the answer instead of every bounce
 finalDistance = round(distance + height, 2)
-print("The", ball, "bounced", bounce, "times, and travelled", finalDistance, "meters.")
+# print("The", ball, "bounced", bounce, "times, and travelled", finalDistance, "meters.")
+print(f"The {ball} bounced {bounce} times, reached a maximum height of {height} meters, "
+      f"and travelled a total distance of {finalDistance} meters. "
+      f"The coefficient of restitution for the chosen ball was {coReChoice}.")

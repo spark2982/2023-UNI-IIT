46f4d956fc2daaf5967337a2aa1362a0cb1ae696
spark2982 <spark2982@gmail.com>
2023-03-29 15:17:36 +1100

    Challenge 1

--- Challenge1.py
+++ Challenge1.py
@@ -4,7 +4,7 @@
 faceValue = int(input("What is the face value of the bond? "))
 marketPrice = int(input("What is the current market price? "))
 yearsToMature = int(input("How many years until it matures? "))
-interest = float(input("What is the interest earned per year? ")) * 1000
+interest = float(input("What is the coupon interest rate? ")) * 1000
 
 # Calculation of a and b
 a = (faceValue - marketPrice) / yearsToMature
